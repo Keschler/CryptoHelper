a8c14f786343027cb4974eafa50d77492ae1cb31
Keschler <62390998+Keschler@users.noreply.github.com>
2021-04-04 13:31:41 +0200

    Added new functions

--- main.py
+++ main.py
@@ -28,14 +28,32 @@ class Crypto:
                              vs_currencies=self.currency)
         return price
 
+    def get_coin_info_by_name(self) -> dict:
+        return cg.get_coin_by_id(self.cryptocurrency)
+
     def get_coins_list(self) -> list:
         coins_list = cg.get_coins_list()
         return coins_list
-    
-    def get_coin_history(self, date=packet_date.today()):
+
+    def get_coin_history(self, date=packet_date.today()) -> dict:
         date = packet_date.strftime(date, "%d-%m-%Y")     # Convert date
         coin_history = cg.get_coin_history_by_id(self.cryptocurrency, date)
         return coin_history
 
+    def get_coin_market_chart_by_id(self, days) -> dict:
+        return cg.get_coin_market_chart_by_id(self.cryptocurrency, self.currency, days)
 
-print(Crypto().get_coin_history())
+    def get_indexes(self) -> list:
+        return cg.get_indexes()
+
+    def get_finance_platforms(self) -> list:
+        return cg.get_finance_platforms()
+
+    def get_finance_products(self) -> list:
+        return cg.get_finance_products()
+
+    def get_global_infos(self) -> dict:
+        return cg.get_global()
+
+    def get_search_trending(self) -> dict:
+        return cg.get_search_trending()
